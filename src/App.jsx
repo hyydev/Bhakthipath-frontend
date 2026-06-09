266944bb58e8032f397116c5ed66fde489820b1e
Devesh sharma <devsharma13314@gmail.com>
2026-06-09 23:25:15 +0530

    making the routes private

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
 import { ThemeProvider } from "./context/ThemeContext";
 import PageWrapper from "./components/PageWrapper";
+import { PrivateRoute } from "./components/PrivateRoute";
 
 import SignUpPage from "./features/auth/pages/SignUpPage";
 import OtpPage from "./features/auth/pages/OtpPage";
 import LoginPage from "./features/auth/pages/LoginPage";
 import ProfilePage from "./features/auth/pages/ProfilePage";
+
 import MainLayout from "./layout/MainLayout";
 import BackgroundWrapper from "./layout/BackgroundWrapper";
+
 import LandingPage from "./features/home/pages/LandingPage";
 import ExamplePage from "./features/home/pages/ExamplePage";
+
 import EcommerceHomePage from "./features/Ecommerce/pages/EcommerceHomePage";
 import ShoppingCart from "./features/EcommerceCart/pages/ShoppingCart";
 import CheckoutPage from "./features/EcommerceCart/pages/CheckoutPage";
 import OrderSuccessPage from "./features/EcommerceCart/pages/OrderSuccessPage";
-
-// import { useAuthStore } from "./features/auth/store";
-// import { useEffect } from "react";
 
 import Providers from "./app/Providers";
 
@@ -28,30 +28,8 @@ function App() {
         <BackgroundWrapper>
           <BrowserRouter>
             <Routes>
-              <Route
-                path="/signup"
-                element={
-                  <PageWrapper>
-                    <SignUpPage />
-                  </PageWrapper>
-                }
-              />
-              <Route
-                path="/otp"
-                element={
-                  <PageWrapper>
-                    <OtpPage />
-                  </PageWrapper>
-                }
-              />
-              <Route
-                path="/login"
-                element={
-                  <PageWrapper>
-                    <LoginPage />
-                  </PageWrapper>
-                }
-              />
+
+              {/* Public Routes */}
               <Route
                 path="/"
                 element={
@@ -60,9 +38,43 @@ function App() {
                   </PageWrapper>
                 }
               />
-              <Route path="/example" element={<ExamplePage />} />
 
+              <Route
+                path="/signup"
+                element={
+                  <PageWrapper>
+                    <SignUpPage />
+                  </PageWrapper>
+                }
+              />
+
+              <Route
+                path="/otp"
+                element={
+                  <PageWrapper>
+                    <OtpPage />
+                  </PageWrapper>
+                }
+              />
+
+              <Route
+                path="/login"
+                element={
+                  <PageWrapper>
+                    <LoginPage />
+                  </PageWrapper>
+                }
+              />
+
+              <Route
+                path="/example"
+                element={<ExamplePage />}
+              />
+
+              {/* Main Layout */}
               <Route element={<MainLayout />}>
+
+                {/* Public Route */}
                 <Route
                   path="/ecommerce"
                   element={
@@ -71,39 +83,48 @@ function App() {
                     </PageWrapper>
                   }
                 />
-                <Route
-                  path="/profile"
-                  element={
-                    <PageWrapper>
-                      <ProfilePage />
-                    </PageWrapper>
-                  }
-                />
-                <Route
-                  path="/cart"
-                  element={
-                    <PageWrapper>
-                      <ShoppingCart />
-                    </PageWrapper>
-                  }
-                />
-                <Route
-                  path="/checkout"
-                  element={
-                    <PageWrapper>
-                      <CheckoutPage />
-                    </PageWrapper>
-                  }
-                />
-                <Route
-                  path="/order-success"
-                  element={
-                    <PageWrapper>
-                      <OrderSuccessPage />
-                    </PageWrapper>
-                  }
-                />
+
+                {/* Protected Routes */}
+                <Route element={<PrivateRoute />}>
+                  <Route
+                    path="/profile"
+                    element={
+                      <PageWrapper>
+                        <ProfilePage />
+                      </PageWrapper>
+                    }
+                  />
+
+                  <Route
+                    path="/cart"
+                    element={
+                      <PageWrapper>
+                        <ShoppingCart />
+                      </PageWrapper>
+                    }
+                  />
+
+                  <Route
+                    path="/checkout"
+                    element={
+                      <PageWrapper>
+                        <CheckoutPage />
+                      </PageWrapper>
+                    }
+                  />
+
+                  <Route
+                    path="/order-success"
+                    element={
+                      <PageWrapper>
+                        <OrderSuccessPage />
+                      </PageWrapper>
+                    }
+                  />
+                </Route>
+
               </Route>
+
             </Routes>
           </BrowserRouter>
         </BackgroundWrapper>
